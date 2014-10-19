4e4ebb7c8a59f3039a2e8ed71939aed2c9e4bf86
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2014-10-19 14:33:55 +0530

    added m-file, its able to do the smple task of acquiring a live feed from the camera right now

---
+++ readCamera.m
@@ -0,0 +1,18 @@
+% copied from Sujeath's code on 19-OCT-2014
+% This code can successfully take a stream from a webcam attached to a computer (windows) and display to screen
+
+vid = videoinput('winvideo', 1,'YUY2_320x240');          % Video Parameters
+set(vid,'ReturnedColorSpace','grayscale');      % acquire in greyscale
+triggerconfig(vid, 'manual');					% the magic sauce that tells matlab that the triggering for 'getsnapshot' will be manual, which helps us speed it up
+
+start(vid);
+gcf = figure;
+
+set(gcf,'CloseRequestFcn',@my_closefcn)			% setting the close request 
+hold on;										% image will persist. to close from command line can use delete(gcf)
+closeflag = 1;
+
+while(closeflag)
+    imshow(getsnapshot(vid));
+    pause(0.01);
+end% Preview
