37c4051a57a1861b3a7d947038e44bf160d77942
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2014-10-19 14:32:20 +0530

    did something dunno

--- readCamera.py
+++ readCamera.py
@@ -3,14 +3,14 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)		# creating the videocapture object
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)	# greyscale
 
     # Display the resulting frame
     cv2.imshow('frame',gray)
