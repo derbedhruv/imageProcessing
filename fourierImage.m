b08655897246abfb4419b73655aaa7d95a5b1e24
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2014-10-20 15:13:53 +0530

    playing with subplottery

--- fourierImage.m
+++ fourierImage.m
@@ -8,10 +8,10 @@ img = imread('pic.jpg');        % read in the pic into an array
 gimg = rgb2gray(img);
 
 % next we display the two images side by side. ref: http://www.mathworks.in/help/images/displaying-images-using-the-imshow-function.html
-subplot(2,2,1), imshow(img,'Border','tight');   % read above link to find out about subplots and even imshow()
-subplot(2,2,2), imshow(gimg,'Border','tight');
+subplot(3,2, 1), imshow(img,'Border','tight');   % read above link to find out about subplots and even imshow()
+subplot(3,2,2), imshow(gimg,'Border','tight');
 
 % next we find the fourier transform of the greyscaled image...http://biocomp.cnb.csic.es/~coss/Docencia/ImageProcessing/Tutorial/
-F = fft2(gimg,512,512);
+F = fft2(gimg, 256, 256);
 F = fftshift(F);            % shift the center
-subplot(2,2,[3 4]), imshow(abs(F),[0,100]); colorbar          % asymmetrical arrangement of subplots is done this way http://matlab.izmiran.ru/help/techdoc/ref/subplot.html
+subplot(3,2,[3 6]), imshow(abs(F),[0,100]); colorbar          % asymmetrical arrangement of subplots is done this way http://matlab.izmiran.ru/help/techdoc/ref/subplot.html
