71071d523fb8a06eb9cd2dad29ed60481ab5b697
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2014-10-25 18:32:38 +0530

    playing around with a paint file with black and white rectangular regions

--- fourierImage.m
+++ fourierImage.m
@@ -2,7 +2,11 @@
 % first I'm going to start by reading in an image and converting it to greyscale
 % I'll use imread()
 
-img = imread('pic.jpg');        % read in the pic into an array
+% 24-OCT-2014
+% Trying out standard images to be able to see how the fft changes
+ford = 256;
+
+img = imread('test.jpg');        % read in the pic into an array
 
 % convert to greyscale
 gimg = rgb2gray(img);
@@ -12,6 +16,6 @@ subplot(3,2, 1), imshow(img,'Border','tight');   % read above link to find out a
 subplot(3,2,2), imshow(gimg,'Border','tight');
 
 % next we find the fourier transform of the greyscaled image...http://biocomp.cnb.csic.es/~coss/Docencia/ImageProcessing/Tutorial/
-F = fft2(gimg, 256, 256);
+F = fft2(gimg, ford, ford);
 F = fftshift(F);            % shift the center
 subplot(3,2,[3 6]), imshow(abs(F),[0,100]); colorbar          % asymmetrical arrangement of subplots is done this way http://matlab.izmiran.ru/help/techdoc/ref/subplot.html
