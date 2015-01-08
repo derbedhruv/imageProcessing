7569b9731b34349cb5e53731a28d4f5c38ce85ec
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2015-01-08 17:08:19 +0530

    viewing the fft was never this easy

--- fourierImage.m
+++ fourierImage.m
@@ -8,10 +8,12 @@
 % 04-JAN-2015
 % Following simple advice given on http://stackoverflow.com/questions/13549186/how-to-plot-a-2d-fft-in-matlab
 
+%{
 ford = 256;     % fourier order
 dord = 1024;    % display order
+%}
 
-img = imread('fpm1.png');        % read in an FPM-taken pic into the array
+img = imread('histology2.jpg');        % read in an FPM-taken pic into the array
 
 % convert to greyscale
 gimg = rgb2gray(img);
@@ -28,8 +30,8 @@ f = abs(f);
 % These are usually really small, we will take the log
 f = log(f + 1);		% 1 is added to prevent log(0) from happening
 
-% Next we normalize between 0 and 1
-f = mat2gray(f);
+% Convert to grayscale image
+% f = mat2gray(f);
 
 % Now we're going to display it
-imshow(f)
+imshow(f,[])
