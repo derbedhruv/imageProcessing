d69699a78e62b31f93d73101fe7ee57f629578a3
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2015-01-08 20:44:54 +0530

    added m file will now modify

---
+++ fpmr.m
@@ -0,0 +1,10 @@
+%{ 
+    Reverse FPM trials using MATLAB
+    Author: Dhruv Joshi
+
+    The idea here is to test out the FPM algorithm (and interesting ideas
+    using it) by chopping a high-res image (from the internet, not owned by
+    me) into smaller pieces and therefore being able to reconstruct it
+    using FPM. This should validate the whole algo.
+
+}%
