c151bc693196b462589ede0408cfd975655f7dc7
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2015-01-10 16:50:29 +0530

    getting a colour image but unfortunately the colour is changing

--- ft.py
+++ ft.py
@@ -36,39 +36,36 @@ def circular_mask(shape,centre,radius):
     return circmask*anglemask
 
 # Now we begin the fun...
-im = Image.open(imageName)	# L makes it greyscale
+im = Image.open(imageName)	# full colour yo
+
+# we display the source image first..
+pylab.figure()
+pylab.imshow(im)
+
 image = numpy.array(im)		# convert to numpy array
+final_image = numpy.zeros(image.shape)	# empty array for our final image
 
-'''
-pylab.figure()
-pylab.imshow(image)
+## Hear ye hear ye, this is for splitting shit up into RGB components and then performing individual operations on them yo
+# now we split the image into it's red, green and blue arrays
+for i in range(0,2):						# 0 - red, 1 - green, 2 - blue
+  channel = image[:,:,i]					# split out the channel
+  f = numpy.fft.fftshift(numpy.fft.fft2(channel))		# find 2D FFT
+  # now you can perform some shit on the fourier transform ..
+  # this section chops out a section and displays the modified file
+  mask = circular_mask(image.shape, (image.shape[0]/2, image.shape[1]/2), 600)
+  # f[~mask] = 0						# chop out circular section
+  psd = numpy.abs(f)						# could be either, assuming the transforms are hte same for each chan
+  
+  # now we find the inverses and stitch them together
+  final_image[:,:,i] = numpy.fft.ifft2(numpy.fft.fftshift(f))
 
-print(image.shape)
-print(image.size)
-'''
-
-f = numpy.fft.fftshift(numpy.fft.fft2(image))		# 2D fft done, just like MATLAB
-
-# this segment just displays the plain old ft..
-# f = fftpack.fftshift(f)		# again like MATLAB, shift the origin to the 'center'
-
-'''
-pylab.figure()
-pylab.imshow(numpy.log10(psd+1))
-pylab.show()
-'''
-
-# this section chops out a section and displays the modified file
-mask = circular_mask(image.shape, (image.shape[0]/2, image.shape[1]/2), 600)
-# f[~mask] = 0		# chop out circular section
-psd = numpy.abs(f)
-
-imm = numpy.fft.ifft2(numpy.fft.fftshift(f))
-image_modi = Image.fromarray(imm.astype(numpy.uint8))	# this is still giving complex values, why?
+image_modi = Image.fromarray(final_image.astype(numpy.uint8))	# this is still giving complex values, why?
 
 pylab.figure()
 pylab.imshow(image_modi)		# display in colour space
 
+'''
 pylab.figure()
 pylab.imshow(numpy.log10(psd+1))
+'''
 pylab.show()
