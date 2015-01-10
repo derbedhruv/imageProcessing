2d57335dd38ef58272c46be7b8368688b85fe451
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2015-01-10 10:47:08 +0530

    moved images into folders and added rgb2gray function to py

--- ft.py
+++ ft.py
@@ -5,11 +5,23 @@ imageName = "cos1.png"
 from scipy import fftpack
 import numpy, Image, pylab
 
+# There's no inbuilt function in any of these python libraries to convert from rgb to grayscale
+# This one was taken from http://stackoverflow.com/questions/12201577/convert-rgb-image-to-grayscale-in-python
+def rgb2gray(rgb):
+    # obv rgb has to be a numpy array
+    r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+    gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+
+    return gray
+
+# Now we begin the fun...
 im = Image.open(imageName)
 image = numpy.array(im)		# convert to numpy array
 
 print(image.shape)
 print(image.size)
+
+# image = rgb2gray(image)
 
 f = fftpack.fft2(image)		# 2D fft done, just like MATLAB
 
