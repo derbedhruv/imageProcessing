d569c14007644db10fd151a444e35f261b132f9c
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2015-01-10 09:50:43 +0530

    ft.py small changes to see basic cosine images and confirm if the fft is indeed working

--- ft.py
+++ ft.py
@@ -1,20 +1,21 @@
 # This shall display the Fourier transform of an image. Really simple code
 # FIRST al the declarations...
-imageName = "eye.jpg"
+imageName = "cos1.png"
 
-import numpy, fftpack, Image, pylab
+from scipy import fftpack
+import numpy, Image, pylab
 
 im = Image.open(imageName)
 image = numpy.array(im)		# convert to numpy array
 
-# print(image.shape)
-# print(image.size)
+print(image.shape)
+print(image.size)
 
 f = fftpack.fft2(image)		# 2D fft done, just like MATLAB
 
 f = fftpack.fftshift(f)		# again like MATLAB, shift the origin to the 'center'
 
-psd = abs(f)
+psd = numpy.abs(f)
 
-pylab.imshow(numpy.log10(psd)
+pylab.imshow(numpy.log10(psd + 1))
 pylab.show()
