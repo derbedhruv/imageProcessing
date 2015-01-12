440c076587728d918e9c915dc189cc57f83aef74
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2015-01-12 23:00:53 +0530

    will contunue fpm recontruction tomorrow

--- fpm.py
+++ fpm.py
@@ -8,7 +8,8 @@
 # Where each step indicates a step in 50 in the fourier domain. Remember that 11 is the actual (0,0).
 import numpy, pylab, Image, scipy.misc
 
-# So we'll start by upsampling the one we know is the 'central' one, i.e. who's FT circular mask is in the center
+#### So we'll start by upsampling the one we know is the 'central' one, i.e. who's FT circular mask is in the center
+####
 folder = "./images/fpm_artificial/"	# the folder where the files of the name given above lie..
 filetype = ".jpg"
 
@@ -24,11 +25,15 @@ pylab.imshow(central_image)
 upsampled = scipy.misc.imresize(ncentral, (3072, 4096))
 upsampled_guess_image = Image.fromarray(upsampled)
 
+'''
 # and we show it
 pylab.figure
 pylab.imshow(upsampled_guess_image)
+'''
 
 # and we save it
 upsampled_guess_image.save(folder + 'upsampled' + filetype)
 
-pylab.show()
+# pylab.show()
+
+#### 
