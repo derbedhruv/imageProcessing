99ffae07ec8797216237f3c43ecbaf88790e7984
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2015-02-25 23:28:21 +0530

    wrote the function to replace intensity in generated low intensity image

--- fpm.py
+++ fpm.py
@@ -32,10 +32,18 @@
 # The camera used for imaging was a Jenoptik ProgRes C3, with 416x308 pixel resolution of images taken. Pixel dimensions are 3.45 micron^2 
 # The sensor size is 7.58 mm x 6.54 mm, and the max resolution attainable is 2080 x 1542 pixels 
 #   
+## we assume that the authors are completely correct in their assumption that the shift in the fourier domain shall be
+# precisely (kx, ky), where these correpond to the wavevectors of each LED source.
+# The wavevector has ampitude equal to the wavenumber in each cartesian axis
+# The wavevector (kx, ky, kz) has a direction given by a unit vector from the light source to the point of illumination on the sample - this
+# will be taken as the origin in 3D cartesian coordinates in this frame of reference.
+#
+# The wavevector for the LED at (a,b,-l) is (-l is fixed since the LED array lies in the z=-l plane
+# (a, b, -l)*(2*pi/{lambda*sqrt(a^2 + b^2 + l^2)}). 
+#
 
 ###### importing required modules
-import numpy, pylab, Image, scipy.misc, matplotlib.cm as cm
-
+import numpy, pylab, Image, scipy.misc, matplotlib.cm as cm, os
 
 ###### UNIVERSAL DEFINITIONS
 ## Experimental setup constants
@@ -48,7 +56,7 @@ origin = [26.44, 20.82]	# the origin w.r.t. the top left of the LED array (as se
 lmbda = 623		# dominant wavelength of the monochromatic light source, in nm
 n = 8			# single dimension of the (square) LED array.
 
-## mathematical constants
+## mathematical and physical constants
 pi = 3.141592		# apple pie
 
 ## System constants
@@ -60,26 +68,9 @@ filename = "00"		# the chosen file to be the upsampled guess image, or starting 
 ## definitions which derive from these universal definitions..
 led_array = numpy.empty([n,n], dtype=object)	# 'object' because we will be making a 2d array of tuples
 wave_number = 2*pi/lmbda
-upsampling_scaling_factor = 2
+upsampling_scaling_factor = 2		# the scaling factor by which we will enhance the resolution of the image
 
-## Now the math part. THis will relate a displacement of (x,y) in the LED aray plane to a shift (kx, ky) in the fourier domain. The 
-# mask for this shift will be a circle in fourier space with a radius of 2*pi*NA/lambda 
-# first we have a for loop which generates the precise positions of the LEDs in an array
-for i in range(0,n):
-  for j in range(0,n):
-    led_array[i][j] = [round(x + d*i - origin[0], 2), round(y + d*j - origin[1], 2)]	# tuple of (x,y) from origin in mm
-
-## Next we assume that the authors are completely correct in their assumption that the shift in the fourier domain shall be
-# precisely (kx, ky), where these correpond to the wavevectors of each LED source.
-# The wavevector has ampitude equal to the wavenumber in each cartesian axis
-# The wavevector (kx, ky, kz) has a direction given by a unit vector from the light source to the point of illumination on the sample - this
-# will be taken as the origin in 3D cartesian coordinates in this frame of reference.
-#
-# The wavevector for the LED at (a,b,-l) is (-l is fixed since the LED array lies in the z=-l plane
-# (a, b, -l)*(2*pi/{lambda*sqrt(a^2 + b^2 + l^2)}). 
-# Simple enough relationship.
-
-
+### FUNCTION DEFINITIONS
 # the circular_mask was copied from http://stackoverflow.com/questions/18352973/mask-a-circular-sector-in-a-numpy-array
 def circular_mask(shape,centre,radius):
     """
@@ -111,6 +102,20 @@ def circular_mask(shape,centre,radius):
 
     return circmask*anglemask
 
+## Next we have a function that replaces the sqrt of Intensity of the complex image expressed in the form sqrt(I)*exp(j*Phase) 
+# and returns an image back again. Since this comes from the phase retreival algorithm developed by Fienup, we will name it after him.
+def fienup_intensity_replace(source_image, replacement_intensity_image):
+  shape = source_image.shape
+  target_image = numpy.zeros(source_image.shape)	# the target image which shall be returned
+  # Now remember that the source image is an mxn complex image. We will cycle through each pixel
+  for m in range(0, shape[0]):
+    for n in range(0, shape[1]):
+      current_complex_pixel = source_image[i][j]
+      current_complex_pixel_intensity = numpy.abs(current_complex_pixel)	# find the sqrt(a2 + b2) of a + ib
+      target_image[i][j] = numpy.abs(replacement_intensity_image)*current_complex_pixel/current_complex_pixel_intensity
+  
+  return target_image
+
 
 #### So we'll start by upsampling the one we know is the 'central' one, i.e. who's FT circular mask is in the center
 ####
@@ -137,3 +142,18 @@ starting_ft_image.save(saving_folder + 'starting_guess_ft' + filetype)
 print("FT of the upsampled image calculated and saved. Now begins the stitching process.")
 
 #### C'est l'heure.
+## Now the iterative stitching part. THis will relate a displacement of (x,y) in the LED aray plane to a shift (kx, ky) in the fourier domain.
+# The mask for this shift will be a circle in fourier space with a radius of 2*pi*NA/lambda 
+# first we have a for loop which loops over all the LEDs in the array
+
+for i in range(0,n):	# x-direction
+  for j in range(0,n):	# y-direction
+    # start by checking whether a particular illumination (LED) file exists, if not move on
+    if (os.path.isfile(reading_folder + str(i) + str(j) + filetype):
+      # Now we can move on
+      led_array[i][j] = [round(x + d*i - origin[0], 2), round(y + d*j - origin[1], 2)]	# tuple of (x,y) from origin in mm
+      
+
+
+
+
