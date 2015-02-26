92aa164c483fc23632f9301d395687a5f2e38462
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2015-02-26 09:02:24 +0530

    downsampling the image once the FT has been chopped yo

--- ft/upsample_ft.py
+++ ft/upsample_ft.py
@@ -34,16 +34,19 @@ def circular_mask(shape,centre,radius):
     return circmask*anglemask
 
 ft_mask = circular_mask(ft.shape, [ft.shape[0]/2 + 200, ft.shape[1]/2], 200)
-print(ft_mask.shape)
+# print(ft_mask.shape)
 
 ft = numpy.fft.fftshift(ft)
 ft[~ft_mask] = 0
 
 chopped_ft = 20*numpy.log(numpy.abs(ft) + 1)
-print(chopped_ft.shape)
+# print(chopped_ft.shape)
 
 chopped_ifft = numpy.fft.ifft2(numpy.fft.fftshift(ft))
+print(chopped_ifft.shape)
+
 chopped_image = Image.fromarray(chopped_ifft.astype(numpy.uint8))
+chopped_image = scipy.misc.imresize(chopped_image, (imar.shape[0], imar.shape[1]))
 
 pylab.figure()
 pylab.imshow(chopped_ft, cmap = cm.Greys_r)
