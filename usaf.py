22f4dd06eaf4352f86aa6a1907e93f9a44d3b44d
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2015-03-04 17:40:18 +0530

    normalized the image properly now its looking like a properly lowpass filtered image

--- usaf.py
+++ usaf.py
@@ -20,40 +20,28 @@ def circular_mask(shape,centre,radius):
     `angle_range` should be given in clockwise order.
     """
     angle_range = [0,360]
-
     x,y = numpy.ogrid[:shape[0],:shape[1]]
     cx,cy = centre
     tmin,tmax = numpy.deg2rad(angle_range)
-
     # ensure stop angle > start angle
     if tmax < tmin:
             tmax += 2*numpy.pi
-
     # convert cartesian --> polar coordinates
     r2 = (x-cx)*(x-cx) + (y-cy)*(y-cy)
     theta = numpy.arctan2(x-cx,y-cy) - tmin
-
     # wrap angles between 0 and 2*pi
     theta %= (2*numpy.pi)
-
     # circular mask
     circmask = r2 <= radius*radius
-
     # angular mask
     anglemask = theta <= (tmax-tmin)
-
     return circmask*anglemask
 
-# this will plot a gaussian function and then we shall convert this into a lowpass filter with precisely known cutoff
-
-import numpy, matplotlib.cm as cm, pylab
-
-# this one's taken and modified from http://stackoverflow.com/questions/14873203/plotting-of-1-dimensional-gaussian-distribution-function
-
 def gaussian(x, offset, fwhm):
+  # this one's taken and modified from http://stackoverflow.com/questions/14873203/plotting-of-1-dimensional-gaussian-distribution-function
   # 'x' is the 1-D space on which the gaussian function will be applied
   sig = fwhm/2.3548   # relating sigma and the FWHM http://mathworld.wolfram.com/GaussianFunction.html
-  return (1/sig*numpy.power(2*numpy.pi, 0.5))*numpy.exp(-numpy.power(x - offset, 2.) / (2 * numpy.power(sig, 2.)))
+  return (1./sig*numpy.power(2.*numpy.pi, 0.5))*numpy.exp(-numpy.power(x - offset, 2.) / (2. * numpy.power(sig, 2.)))
 
 def gaussian_filter(centre, shape, width, h_max):
   # a 2d gaussian at centre (a tuple) and with width in both dimensions as 'width', with height h_max
@@ -73,11 +61,11 @@ def band_stop(target_image, freq, spread):
   
   # convert the 'frequency' into pixels
   nyquist_freq = 1.00/(2*pixel_size)		# arb. units, this is the max freqyency which can be resolved by this fft
-  frequency = (freq/nyquist_freq)*(target_image.shape[1]/2)
+  frequency = 2.*(freq/nyquist_freq)*(target_image.shape[1]/2)
   
   # define two circular masks with radius (frequency - spread) and (frequency + spread)
-  inner = circular_mask(target_image.shape, center, (frequency - spread))
-  outer = circular_mask(target_image.shape, center, (frequency + spread))
+  # inner = circular_mask(target_image.shape, center, (frequency - spread))
+  # outer = circular_mask(target_image.shape, center, (frequency + spread))
 
   ## now apply these masks and generate the target fourier transform
   # first we find the fourier transform
@@ -85,14 +73,17 @@ def band_stop(target_image, freq, spread):
   
   # then we add together the result of applying both masks
   band_stopped_fft = numpy.copy(f)
-  f_inner = numpy.copy(f)
-  
-  band_stopped_fft[outer] = 0
+
+  #f_inner = numpy.copy(f)
+  # band_stopped_fft[outer] = 0
   # f_inner[inner] = 0
   # f_inner = f - f_inner
   # band_stopped_fft = band_stopped_fft + f_inner
-  band_stopped_fft = f - band_stopped_fft
+  # band_stopped_fft = f - band_stopped_fft
   
+  bs_filt = gaussian_filter(center, target_image.shape, frequency, 1) # 2*freq because it is symmetrical about the center and we're giving the spread
+  band_stopped_fft = band_stopped_fft*bs_filt
+
   # display it so we know it's all good..
   pylab.figure()
   p = numpy.log(numpy.abs(band_stopped_fft) + 1)
@@ -100,7 +91,10 @@ def band_stop(target_image, freq, spread):
   pylab.imshow(p, cmap=cm.Greys_r)
 
   # then calculate the inverse fft of it and return..
-  band_stopped_image = numpy.fft.ifft2(numpy.fft.fftshift(band_stopped_fft))
+  band_stopped_image = numpy.abs(numpy.fft.ifft2(numpy.fft.fftshift(band_stopped_fft)))
+  
+  # we then normalize all the values to (0,255)
+  band_stopped_image = 255*band_stopped_image/(numpy.max(band_stopped_image))
 
   # return it..
   return band_stopped_image
@@ -108,26 +102,14 @@ def band_stop(target_image, freq, spread):
 ## now the section where we apply everything..
 # we start by calculating the frequency where we would like to remove (dependent on the object size)
 # first we need the size of the object we want to attenuate
-object_size = 50.0	# microns
+object_size = 100.0	# microns
 
 # freqyency calculatins...
 object_frequency = 1.00/(2*object_size)
 
-# then apply the mask..
+# then apply the circular frequency mask..
 final_image = band_stop(usaf, object_frequency, 1)
 final = Image.fromarray(final_image.astype(numpy.uint8))
-
-# applying the gaussian mask..
-shape = [100,100]
-width = 10
-center = [shape[0]/2, shape[1]/2]
-h_max = 1
-
-pylab.figure()
-pylab.imshow(gaussian_filter(center, shape, width, h_max), cmap=cm.Greys_r)
-pylab.show()
-
-
 
 # then display the filtered image
 pylab.figure()
