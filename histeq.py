006f4a37011016c594179161dafbb5ae4f5bc948
Dhruv Joshi LVP <dhruv.joshi.1989@gmail.com>
2015-03-13 18:40:45 +0530

    added histogram equalization file

---
+++ histeq.py
@@ -0,0 +1,10 @@
+# This will perform a histogram equalization
+# taken from http://www.janeriksolem.net/2009/06/histogram-equalization-with-python-and.html
+
+def histeq(im,nbr_bins=256):
+  imhist,bins = numpy.histogram(im.flatten(),nbr_bins,normed=True)
+  cdf = imhist.cumsum() #cumulative distribution function
+  cdf = 255 * cdf / cdf[-1] #normalize
+  im2 = numpy.interp(im.flatten(),bins[:-1],cdf)
+  return im2.reshape(im.shape), cdf
+
